39b95ca46f42f1bf8d5297ab6a1c3dd53c1a7c84
timkari <ktm332@mail.ru>
2024-03-04 13:49:19 +0300

    идфидфидфэид

--- prac2/app/src/main/java/com/example/prac2/MainActivity.java
+++ prac2/app/src/main/java/com/example/prac2/MainActivity.java
@@ -1,24 +1,63 @@
 package com.example.prac2;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Build;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        Log.d("RRR","onCreate()");
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        String androidVersion = getString(R.string.version, Build.VERSION.SDK_INT);
+        TextView textViewVersion = findViewById(R.id.version);
+        textViewVersion.setText(androidVersion);
+
+
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("SET", "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("SET", "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("SET", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("SET", "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("SET", "onRestart: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("SET", "onDestroy: ");
+    }
+
 }
